da19ec2ae019a1a942ff80e2a51b69cd68bbc869
lamorasjr <lamorasjr@gmail.com>
2024-10-27 03:46:36 +0100

    updated main code with full pipeline

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
-import os
-from datasets import pipeline_datasets
-from measures_info import pipeline_measures_info
+from etl_pbi import export_pbi_in_group, get_datasets_ids
+from etl_pbi_info import etl_pbi_info
 
-if __name__ == '__main__':
-    pipeline_datasets()
-    pipeline_measures_info('data/datasets.csv')
+pbi_objects = ["datasets", "dataflows", "dashboards", "reports"]
+dataset_ids = get_datasets_ids()
+dax_query = 'EVALUATE INFO.MEASURES()'
+file_name = 'measures_info'
+
+for i in pbi_objects:
+    export_pbi_in_group(i)
+
+etl_pbi_info(dataset_ids, dax_query, file_name)
