96de090d22b8269908d843ffe8feefa06d3b9c8f
lamorasjr <lamorasjr@gmail.com>
2025-01-04 23:35:44 +0100

    updated powerbi_api_functions

--- src/powerbi_api_functions.py
+++ src/powerbi_api_functions.py
@@ -1,13 +1,16 @@
 import os
 import requests
 
+PBI_WORKSPACE_ID = os.getenv('PBI_WORKSPACE_ID')
+PBI_ACCESS_TOKEN = os.getenv('PBI_ACCESS_TOKEN')
+
 # Extract list of datasets contained in the Power BI Workspace
-def get_datasets_in_workspace(access_token:str, workspace_id:str):
+def get_datasets_in_workspace():
     """
-    Function to extract all datasets contained in a given Power BI workspace.
+    Function to extract all datasets in the Power BI workspace set in the env.
     """
-    url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/datasets'
-    headers = { 'Authorization' : f'Bearer {access_token}'}
+    url = f'https://api.powerbi.com/v1.0/myorg/groups/{PBI_WORKSPACE_ID}/datasets'
+    headers = { 'Authorization' : f'Bearer {PBI_ACCESS_TOKEN}'}
 
     response = requests.get(url, headers=headers)
 
@@ -19,14 +22,14 @@ def get_datasets_in_workspace(access_token:str, workspace_id:str):
         raise Exception(f'Non-success status code: {response.status_code}')
 
 
-def get_datasets_dax_info(access_token:str, workspace_id:str, dataset_id:str, dax_query:str):
+def get_datasets_dax_info(dataset_id:str, dax_query:str):
     """
     Function to execute a given DAX query into a given Power BI dataset in a specific workspace.
     """
-    url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/datasets/{dataset_id}/executeQueries'
+    url = f'https://api.powerbi.com/v1.0/myorg/groups/{PBI_WORKSPACE_ID}/datasets/{dataset_id}/executeQueries'
 
     headers = { 
-        'Authorization' : f'Bearer {access_token}',
+        'Authorization' : f'Bearer {PBI_ACCESS_TOKEN}',
         'Content-type' : 'application/json'
     }
 
@@ -47,17 +50,11 @@ def get_datasets_dax_info(access_token:str, workspace_id:str, dataset_id:str, da
 
 
 if __name__ == '__main__':
-    pbi_workspace_id = os.getenv('PBI_WORKSPACE_ID')
-    pbi_access_token = os.getenv('PBI_ACCESS_TOKEN')
-    pbi_sample_dataset_id = os.getenv('SAMPLE_DATASET_ID')
-    pbi_sample_dax_query = 'EVALUATE INFO.TABLES()'
+    test_dax_query = 'EVALUATE INFO.TABLES()'
 
-    pbi_datasets = get_datasets_in_workspace(access_token=pbi_access_token, workspace_id=pbi_workspace_id)
+    pbi_datasets = get_datasets_in_workspace()
     # print(pbi_datasets)
-    
 
-    pbi_tables = get_datasets_dax_info(access_token=pbi_access_token, 
-                                      workspace_id=pbi_workspace_id, 
-                                      dataset_id = pbi_sample_dataset_id, 
-                                      dax_query=pbi_sample_dax_query)
+    test_dataset = pbi_datasets[0]['id']
+    pbi_tables = get_datasets_dax_info(dataset_id=test_dataset, dax_query=test_dax_query)
     # print(pbi_tables)
