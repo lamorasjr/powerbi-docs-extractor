2f5b488c5eb480b0e7df790fbf646fbba2171db1
lamorasjr <lamorasjr@gmail.com>
2025-01-04 23:36:17 +0100

    added etl functions for tables, columns, measures and relationships

--- src/etl.py
+++ src/etl.py
@@ -1,13 +1,13 @@
 import os
 import pandas as pd
-from powerbi_api_functions import get_datasets_in_workspace #, get_datasets_dax_info
-
-PBI_WORKSPACE_ID = os.getenv('PBI_WORKSPACE_ID')
-PBI_ACCESS_TOKEN = os.getenv('PBI_ACCESS_TOKEN')
+from powerbi_api_functions import get_datasets_in_workspace, get_datasets_dax_info
 
 # ETL function for pbi datasets
 def etl_pbi_datasets():
-    data = get_datasets_in_workspace(access_token=PBI_ACCESS_TOKEN, workspace_id=PBI_WORKSPACE_ID)
+    """
+    Extract all Power BI datasets from a workspace, transform and load in a Pandas DataFrame.
+    """
+    data = get_datasets_in_workspace()
     df = pd.json_normalize(data)
     df_etl = df[['id', 'name', 'webUrl', 'createdDate']]
     df_etl = df_etl.rename(columns={'id':'DATASET_ID',
@@ -19,15 +19,79 @@ def etl_pbi_datasets():
     return df_etl
 
 # ETL function for pbi dataset tables
+def etl_pbi_tables(dataset_id:str):
+    """
+    Extract all tables from a Power BI dataset, transform and load in a Pandas DataFrame.
+    """
+    with open(f'dax_queries/tables_info.txt', 'r') as f:
+        dax_query = f.read()
+
+    data = get_datasets_dax_info(dataset_id=dataset_id, dax_query=dax_query)
+    df = pd.json_normalize(data)
+    return df
+
+
 # ETL function for pbi dataset columns
+def etl_pbi_columns(dataset_id:str):
+    """
+    Extract all columns from a Power BI dataset, transform and load in a Pandas DataFrame.
+    """
+    with open(f'dax_queries/columns_info.txt', 'r') as f:
+        dax_query = f.read()
+
+    data = get_datasets_dax_info(dataset_id=dataset_id, dax_query=dax_query)
+    df = pd.json_normalize(data)
+    return df
+
+
 # ETL function for pbi dataset measures
+def etl_pbi_measures(dataset_id:str):
+    """
+    Extract all measures from a Power BI dataset, transform and load in a Pandas DataFrame.
+    """
+    with open(f'dax_queries/measures_info.txt', 'r') as f:
+        dax_query = f.read()
+
+    data = get_datasets_dax_info(dataset_id=dataset_id, dax_query=dax_query)
+    df = pd.json_normalize(data)
+    return df
+
+
 # ETL function for pbi dataset relationships
+def etl_pbi_relationships(dataset_id:str):
+    """
+    Extract all relationships from a Power BI dataset, transform and load in a Pandas DataFrame.
+    """
+    with open(f'dax_queries/relationships_info.txt', 'r') as f:
+        dax_query = f.read()
 
-def export_pbi_raw_data():
-    df_datasets.to_csv('raw_data/pbi_datasets_raw.csv', index=False, sep=';', encoding='utf-8')
-
-    print('The raw data files were exported successfully')
+    data = get_datasets_dax_info(dataset_id=dataset_id, dax_query=dax_query)
+    df = pd.json_normalize(data)
+    return df
 
 if __name__ == '__main__':
     df_datasets = etl_pbi_datasets()
-    export_pbi_raw_data()
+    # print(df_datasets.head())
+
+    test_dataset = df_datasets['DATASET_ID'].iloc[0]
+    
+    df_tables = etl_pbi_tables(dataset_id=test_dataset)
+    # print(df_tables.head(2))
+
+    df_columns = etl_pbi_columns(dataset_id=test_dataset)
+    # print(df_columns.head(2))
+    
+    df_measures = etl_pbi_measures(dataset_id=test_dataset)
+    # print(df_measures.head(2))
+    
+    df_relationships = etl_pbi_relationships(dataset_id=test_dataset)
+    # print(df_relationships.head(2))
+
+
+    # Export raw data to csv
+    df_datasets.to_csv('data_raw/pbi_datasets_raw.csv', index=False, sep=';', encoding='utf-8')
+    df_tables.to_csv('data_raw/pbi_tables_raw.csv', index=False, sep=';', encoding='utf-8')
+    df_columns.to_csv('data_raw/pbi_columns_raw.csv', index=False, sep=';', encoding='utf-8')
+    df_measures.to_csv('data_raw/pbi_measures_raw.csv', index=False, sep=';', encoding='utf-8')
+    df_relationships.to_csv('data_raw/pbi_relationships_raw.csv', index=False, sep=';', encoding='utf-8')
+    print('The raw data files were exported successfully')
