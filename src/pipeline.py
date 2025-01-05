d29d02b7fcb06bbe5fb10624bee128bbec6d7126
lamorasjr <lamorasjr@gmail.com>
2025-01-05 23:15:39 +0100

    added pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,49 +1,46 @@
 import pandas as pd
 from etl import etl_pbi_datasets, etl_dataset_tables, etl_dataset_columns, etl_dataset_measures, etl_dataset_relationships
 
-def generate_datasets_ids():
-    """
-    Returns a list with ids of all datasets in the workspace
-    """
-    df_datasets = etl_pbi_datasets()
-    datasets_ids = list(df_datasets['DATASET_ID'])
-    return datasets_ids
+def etl_pbi_tables(dataset_ids:list) -> pd.DataFrame:
+    df_list = [ etl_dataset_tables(dataset_id=id) for id in dataset_ids ]
+    df = pd.concat(df_list, ignore_index=True)
+    return df
 
-def build_pbi_tables_info():
-    """
-    Extract, transform and combine all tables from all datasets to a Pandas dataframe.
-    """
-    datasets_ids = generate_datasets_ids()
-    df_list = [ etl_dataset_tables(dataset_id=id) for id in datasets_ids ]
-    df_total = pd.concat(df_list, ignore_index=True)
-    return df_total
+def etl_pbi_columns(dataset_ids:list) -> pd.DataFrame:
+    df_list = [ etl_dataset_columns(dataset_id=id) for id in dataset_ids ]
+    df = pd.concat(df_list, ignore_index=True)
+    return df
 
-def build_pbi_columns_info():
-    """
-    Extract, transform and combine all columns from all datasets to a Pandas dataframe.
-    """
-    datasets_ids = generate_datasets_ids()
-    df_list = [ etl_dataset_columns(dataset_id=id) for id in datasets_ids ]
-    df_total = pd.concat(df_list, ignore_index=True)
-    return df_total
+def etl_pbi_measures(dataset_ids:list) -> pd.DataFrame:
+    df_list = [ etl_dataset_measures(dataset_id=id) for id in dataset_ids ]
+    df = pd.concat(df_list, ignore_index=True)
+    return df
+
+def etl_pbi_relationships(dataset_ids:list) -> pd.DataFrame:
+    df_list = [ etl_dataset_relationships(dataset_id=id) for id in dataset_ids ]
+    df = pd.concat(df_list, ignore_index=True)
+    return df
 
 
+if __name__ == '__main__':
+    df_pbi_datasets = etl_pbi_datasets()
+    df_pbi_datasets.to_csv('data/pbi_datasets_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI datasets info has been sucessefully exported')
 
-print(build_pbi_columns_info())
+    dataset_ids = list(df_pbi_datasets['DATASET_ID'])
 
+    df_pbi_tables = etl_pbi_tables(dataset_ids)
+    df_pbi_tables.to_csv('data/pbi_datasets_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI tables info has been sucessefully exported')
 
+    df_pbi_columns = etl_pbi_columns(dataset_ids)
+    df_pbi_columns.to_csv('data/pbi_columns_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI columns info has been sucessefully exported')
 
-# df_tables.to_csv('data_raw/pbi_tables_raw.csv', index=False, sep=';', encoding='utf-8')
+    df_pbi_measures = etl_pbi_measures(dataset_ids)
+    df_pbi_measures.to_csv('data/pbi_measures_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI measures info has been sucessefully exported')
 
-# df_columns = etl_pbi_columns(dataset_id=test_dataset)
-# print(df_columns.info())
-# df_columns.to_csv('data_raw/pbi_columns_raw.csv', index=False, sep=';', encoding='utf-8')
-
-# df_measures = etl_pbi_measures(dataset_id=test_dataset)
-# print(df_measures.info())
-# df_measures.to_csv('data_raw/pbi_measures_raw.csv', index=False, sep=';', encoding='utf-8')
-
-# df_relationships = etl_pbi_relationships(dataset_id=test_dataset)
-# print(df_relationships.iloc[0])
-# df_relationships.to_csv('data_raw/pbi_relationships_raw.csv', index=False, sep=';', encoding='utf-8')
-
+    df_pbi_relationships = etl_pbi_relationships(dataset_ids)
+    df_pbi_relationships.to_csv('data/pbi_relationships_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI relationships info has been sucessefully exported')
