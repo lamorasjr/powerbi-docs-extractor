ff1d94542478832eb101ac4d46ad3e3098f4802c
lamorasjr <lamorasjr@gmail.com>
2025-01-12 03:25:15 +0100

    build main script

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
+import os
 import pandas as pd
-import warnings
-from src.pipeline import etl_pbi_tables
+from extract_powerbi_api import get_all_reports, get_all_datasets, get_all_datasets_info
 
-output_path = 'data/'
+def extract_powerbi_data_catalog_info():
+    workspace_ids = os.getenv('PBI_WORKSPACES_IDS').split(",")
 
-# Ignore specific FutureWarning
-warnings.simplefilter(action='ignore', category=FutureWarning)
+    all_reports = get_all_reports(workspace_ids)
+    df_reports = pd.json_normalize(all_reports, record_path='response', meta='workspace_id')
+    df_reports.to_csv('data/pbi_reports_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI reports info has been sucessefully exported')
 
-pbi_dfs = etl_pbi_tables()
+    all_datasets = get_all_datasets(workspace_ids)
+    df_datasets = pd.json_normalize(all_datasets, record_path='response', meta='workspace_id')
+    df_datasets.to_csv('data/pbi_datasets_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI datasets info has been sucessefully exported')
 
-with pd.ExcelWriter(f'{output_path}/powerbi_data_catalog.xlsx') as writer:
-    for sheet_name, df in pbi_dfs.items():
-        df.to_excel(writer, sheet_name=sheet_name, index=False)
+    all_tables = get_all_datasets_info(workspace_ids, 'tables_info.txt')
+    df_tables = pd.json_normalize(all_tables ,record_path='response', meta=['workspace_id', 'dataset_id'])
+    df_tables.to_csv('data/pbi_tables_info.csv', index=False, sep=';', encoding='utf-8')
+    print('Power BI tables info has been sucessefully exported')
 
-print('The powerbi_data_catalog.xlsx file has been exported successfully')
+if __name__ == '__main__':
+    extract_powerbi_data_catalog_info()
