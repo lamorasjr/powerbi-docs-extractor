b05a29a7d406c16c90614ea8aa1c13730dc6bbf8
lamorasjr <lamorasjr@gmail.com>
2025-01-16 02:20:45 +0100

    adjusted code for powerbi_rest_api.py / renamed file loader.py / build etl for all tables in extractor.py

---
+++ src/extractor.py
@@ -0,0 +1,135 @@
+import os
+import re
+import pandas as pd
+from powerbi_rest_api import ( get_auth_token, get_workspaces_info, get_workspace_reports, 
+                               get_workspace_datasets, execute_dataset_query, get_report_pages )
+from loader import load_data
+
+# Extract, transform and load powerbi workspaces
+def etl_powerbi_workspaces(access_token:str, workspaces_ids:list, output_path:str, output_format:str):
+    all_data = []
+
+    for ws_id in workspaces_ids:
+        response = get_workspaces_info(access_token, ws_id)
+        all_data.append(response)
+
+    df_raw = pd.json_normalize(all_data)
+    df = df_raw.copy()
+    df = df[['id', 'name', 'type']]
+    df = df.rename(columns={
+        'id': 'WORKSPACE_ID',
+        'name': 'WORKSPACE_NAME',
+        'type': 'TYPE'
+    })
+    return load_data(df, 'powerbi_workspaces', output_path, output_format)
+
+
+# Extract, transform and load powerbi reports
+def etl_powerbi_reports(access_token:str, workspaces_ids:list, output_path:str, output_format:str):
+    all_data = []
+
+    for ws_id in workspaces_ids:
+        response = get_workspace_reports(access_token, ws_id)
+        all_data.append(response)
+
+    df_raw = pd.json_normalize(all_data, meta='WORKSPACE_ID', record_path='response')
+    df = df_raw.copy()
+    df = df[['id', 'name', 'reportType', 'webUrl', 'WORKSPACE_ID']]
+    df = df.rename(columns={
+        'id':'REPORT_ID', 
+        'name':'REPORT_NAME', 
+        'reportType':'REPORT_TYPE', 
+        'webUrl':'WEB_URL'
+    })
+
+    return load_data(df, 'powerbi_reports', output_path, output_format)
+
+
+# Extract, transform and load powerbi reports pages
+def etl_powerbi_reports_pages(access_token:str, workspaces_ids:list, output_path:str, output_format:str):
+    all_data = []
+
+    for ws_id in workspaces_ids:
+        response = get_workspace_reports(access_token, ws_id)
+        for i in response['response']:
+            rp_id = i['id']
+            reponse_data = get_report_pages(access_token, ws_id, rp_id)
+            all_data.append(reponse_data)
+
+    df_raw = pd.json_normalize(all_data, meta=['WORKSPACE_ID', 'REPORT_ID'], record_path='response')
+    df = df_raw.copy()
+    df = df.rename(columns={
+        'name':'PAGE_SECTION', 
+        'displayName':'PAGE_NAME', 
+        'order':'ORDER'
+    })
+
+    return load_data(df, 'powerbi_reports_pages', output_path, output_format)
+
+
+# Extract, transform and load powerbi datasets
+def etl_powerbi_datasets(access_token:str, workspaces_ids:list, output_path:str, output_format:str):
+    all_data = []
+
+    for ws_id in workspaces_ids:
+        response = get_workspace_datasets(access_token, ws_id)
+        all_data.append(response)
+
+    df_raw = pd.json_normalize(all_data, meta='WORKSPACE_ID', record_path='response')
+    df = df_raw.copy()
+    df = df[['id', 'name', 'webUrl', 'createdDate', 'WORKSPACE_ID']]
+    df = df.rename(columns={
+        'id':'DATASET_ID', 
+        'name':'DATASET_NAME', 
+        'createdDate':'CREATED_DATE',
+        'webUrl':'WEB_URL'
+    })
+    df['CREATED_DATE'] = pd.to_datetime(df['CREATED_DATE']).dt.date
+
+    return load_data(df, 'powerbi_datasets', output_path, output_format)
+
+# Extract, transform and load powerbi dataset dax info queries
+def etl_datasets_dax_queries(access_token:str, workspaces_ids:list, output_path:str, output_format:str):
+    dax_queries_dir = 'src\\dax_queries'
+
+    for q in os.listdir(dax_queries_dir):
+        file_name = q.split(".")[0]
+        file_dir = os.path.join(dax_queries_dir, f'{file_name}.txt')
+
+        with open(file_dir, 'r') as file:
+            file_content = file.read()
+
+        all_data = []
+
+        for ws_id in workspaces_ids:    
+            ds_response = get_workspace_datasets(access_token, ws_id)
+            for i in ds_response['response']:
+                ds_id = i['id']
+                response = execute_dataset_query(access_token, ws_id, ds_id, file_content)
+                all_data.append(response)
+
+        df_raw = pd.json_normalize(all_data, meta=['WORKSPACE_ID', 'DATASET_ID'], record_path='response')
+        df = df_raw.copy()
+        old_columns_name = df.columns
+        new_columns_name = {i: re.sub(r'\[([^\]]+)\]', r'\1', i) for i in old_columns_name}
+        df = df.rename(columns=new_columns_name)
+        df['MODIFIED_DATE'] = pd.to_datetime(df['MODIFIED_DATE']).dt.date
+        
+        load_data(df, f'powerbi_datasets_{file_name}', output_path, output_format)
+
+
+if __name__ == '__main__':
+    TENANT_ID = os.getenv('PBI_TENANT_ID')
+    CLIENT_ID = os.getenv('PBI_CLIENT_ID')
+    CLIENT_SECRET = os.getenv('PBI_CLIENT_SECRET')
+    WORKSPACES_IDS = os.getenv('PBI_WORKSPACES_IDS').split(",")
+    output_path = 'data/'
+    output_format = 'csv'
+
+    access_token = get_auth_token(TENANT_ID, CLIENT_ID, CLIENT_SECRET)
+
+    etl_powerbi_workspaces(access_token, WORKSPACES_IDS, output_path, output_format)
+    etl_powerbi_reports(access_token, WORKSPACES_IDS, output_path, output_format)
+    etl_powerbi_reports_pages(access_token, WORKSPACES_IDS, output_path, output_format)
+    etl_powerbi_datasets(access_token, WORKSPACES_IDS, output_path, output_format)
+    etl_datasets_dax_queries(access_token, WORKSPACES_IDS, output_path, output_format)
--- src/powerbi_rest_api.py
+++ src/powerbi_rest_api.py
@@ -1,7 +1,7 @@
-import os
+import json
 import requests
 
-def get_auth_token(tenant_id:str, client_id:str, client_secret:str):
+def get_auth_token(tenant_id:str, client_id:str, client_secret:str)->json:
     url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
     payload = {
         'grant_type': 'client_credentials',
@@ -9,7 +9,7 @@ def get_auth_token(tenant_id:str, client_id:str, client_secret:str):
         'client_secret': client_secret,
         'scope': 'https://analysis.windows.net/powerbi/api/.default'
     }
-    response = requests.post(url, data=payload)
+    response = requests.post(url=url, data=payload)
     if response.status_code == 200:
         response_json = response.json()
         data = response_json['access_token']
@@ -18,13 +18,13 @@ def get_auth_token(tenant_id:str, client_id:str, client_secret:str):
         raise Exception(f'Failed to obtain token: {response.status_code} - {response.text}')
 
 
-def get_workspaces_info(access_token:str, workspace_id:str):
+def get_workspaces_info(access_token:str, workspace_id:str)->json:
     url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}'
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url=url, headers=headers)
     if response.status_code == 200:
         response_json = response.json()
         data = response_json
@@ -33,13 +33,13 @@ def get_workspaces_info(access_token:str, workspace_id:str):
         raise Exception(f'Failed request - error: {response.status_code} - {response.text}')
 
 
-def get_workspace_reports(access_token:str, workspace_id:str):
+def get_workspace_reports(access_token:str, workspace_id:str)->json:
     url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/reports'
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url=url, headers=headers)
     if response.status_code == 200:
         response_json = response.json()
         data = { 
@@ -51,13 +51,13 @@ def get_workspace_reports(access_token:str, workspace_id:str):
         raise Exception(f'Failed request - error: {response.status_code} - {response.text}')
 
 
-def get_workspace_datasets(access_token:str, workspace_id:str):
+def get_workspace_datasets(access_token:str, workspace_id:str)->json:
     url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/datasets'
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url=url, headers=headers)
     if response.status_code == 200:
         response_json = response.json()
         data = { 
@@ -69,7 +69,7 @@ def get_workspace_datasets(access_token:str, workspace_id:str):
         raise Exception(f'Failed request - error: {response.status_code} - {response.text}')
 
 
-def execute_dataset_query(access_token:str, workspace_id:str, dataset_id:str, dax_query:str):
+def execute_dataset_query(access_token:str, workspace_id:str, dataset_id:str, dax_query:str)->json:
     url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/datasets/{dataset_id}/executeQueries'
     headers = { 
         'Authorization' : f'Bearer {access_token}',
@@ -79,13 +79,32 @@ def execute_dataset_query(access_token:str, workspace_id:str, dataset_id:str, da
         'queries': [{'query': f'{dax_query}'}],
         'serializerSettings': {'includeNulls': 'true'}
     }
-    response = requests.post(url, headers=headers, json=body)
+    response = requests.post(url=url, headers=headers, json=body)
     if response.status_code == 200:
         response_json = response.json()
         data = {
             "WORKSPACE_ID": workspace_id,
             "DATASET_ID": dataset_id,
-            "response" : response_json['results'][0]['tables'][0]['rows'],
+            "response" : response_json['results'][0]['tables'][0]['rows']
+        }
+        return data
+    else:
+        raise Exception(f'Non-sucess status code: {response.status_code} - {response.text}')
+    
+
+def get_report_pages(access_token:str, workspace_id:str, report_id:str)->json:
+    url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/reports/{report_id}/pages'
+    headers = { 
+        'Authorization' : f'Bearer {access_token}',
+        'Content-type' : 'application/json'
+    }
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200:
+        response_json = response.json()
+        data = {
+            "WORKSPACE_ID": workspace_id,
+            "REPORT_ID": report_id,
+            "response" : response_json['value']
         }
         return data
     else:
