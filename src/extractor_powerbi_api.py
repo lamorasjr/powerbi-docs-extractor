e2b9b4f2c9a3cd31f19dfdb87e6dcaf01ea48842
lamorasjr <lamorasjr@gmail.com>
2025-01-30 15:51:29 +0100

    Update auth token functions

--- src/extractor_powerbi_api.py
+++ src/extractor_powerbi_api.py
@@ -2,9 +2,41 @@ import json
 import requests
 import pandas as pd
 import re
+import os
+from dotenv import load_dotenv
 
-def get_powerbi_data(access_token:str, endpoint:str)->json:
+load_dotenv()
+
+def get_powerbi_token():
+    TENANT_ID = os.getenv('AZURE_APP_TENANT_ID')
+    CLIENT_ID = os.getenv('AZURE_APP_CLIENT_ID')
+    CLIENT_SECRET = os.getenv('AZURE_APP_CLIENT_SECRET')
+
+    url = f'https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token'
+    
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'scope': 'https://analysis.windows.net/powerbi/api/.default'
+    }
+
+    response = requests.post(url=url, data=payload)
+    if response.status_code == 200:
+        response_json = response.json()
+        data = response_json['access_token']
+        return data
+    
+    else:
+        raise Exception(f'Failed to obtain token: {response.status_code} - {response.text}')
+
+
+def get_powerbi_data(endpoint:str)->json:
+
+    access_token = get_powerbi_token()
+
     url = f'https://api.powerbi.com/v1.0/myorg/groups/{endpoint}'
+    
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'  
@@ -15,16 +47,22 @@ def get_powerbi_data(access_token:str, endpoint:str)->json:
     if response.status_code == 200:
         data = response.json()
         return data
+    
     else:
         raise Exception(f'Failed request - error: {response.status_code} - {response.text}')
     
     
-def execute_powerbi_dataset_query(access_token:str, workspace_id:str, dataset_id:str, dax_query:str)->dict:
+def execute_powerbi_dataset_query(workspace_id:str, dataset_id:str, dax_query:str)->dict:
+
+    access_token = get_powerbi_token()
+
     url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/datasets/{dataset_id}/executeQueries'
+    
     headers = { 
         'Authorization' : f'Bearer {access_token}',
         'Content-type' : 'application/json'
     }
+
     body = {
         'queries': [{'query': f'{dax_query}'}],
         'serializerSettings': {'includeNulls': 'true'}
@@ -40,16 +78,18 @@ def execute_powerbi_dataset_query(access_token:str, workspace_id:str, dataset_id
             "response" : response_json['results'][0]['tables'][0]['rows']
         }
         return data
+    
     else:
         raise Exception(f'Non-sucess status code: {response.status_code} - {response.text}')
 
 
-def extract_workspaces(access_token:str, workspaces_ids:list)->pd.DataFrame:
+def extract_workspaces(workspaces_ids:list)->pd.DataFrame:
+
     all_data = []
 
     for ws_id in workspaces_ids:
         endpoint = ws_id
-        data = get_powerbi_data(access_token, endpoint)
+        data = get_powerbi_data(endpoint)
         all_data.append(data)
 
     df_raw = pd.json_normalize(all_data)
@@ -60,15 +100,17 @@ def extract_workspaces(access_token:str, workspaces_ids:list)->pd.DataFrame:
         'name': 'WORKSPACE_NAME',
         'type': 'TYPE'
     })
+
     return df
 
 
-def extract_reports(access_token:str, workspaces_ids:list)->pd.DataFrame:
+def extract_reports(workspaces_ids:list)->pd.DataFrame:  
+
     all_data = []
 
     for ws_id in workspaces_ids:
         endpoint = f'{ws_id}/reports'
-        response = get_powerbi_data(access_token, endpoint)
+        response = get_powerbi_data(endpoint)
         data = {
             'WORKSPACE_ID' : ws_id,
             'response' : response.get('value')
@@ -84,11 +126,14 @@ def extract_reports(access_token:str, workspaces_ids:list)->pd.DataFrame:
         'reportType':'REPORT_TYPE', 
         'webUrl':'WEB_URL'
     })
+
     return df
 
 
-def extract_reports_pages(access_token:str, workspaces_ids:list)->pd.DataFrame:
-    df_reports = extract_reports(access_token, workspaces_ids)
+def extract_reports_pages(workspaces_ids:list)->pd.DataFrame:
+
+    df_reports = extract_reports(workspaces_ids)
+    
     df_reports = df_reports[df_reports['REPORT_TYPE'] == 'PowerBIReport']
 
     workspaces_reports_ids = df_reports[['WORKSPACE_ID', 'REPORT_ID']].to_dict(orient='records')
@@ -100,7 +145,7 @@ def extract_reports_pages(access_token:str, workspaces_ids:list)->pd.DataFrame:
         rp_id = i.get('REPORT_ID')
 
         endpoint = f'{ws_id}/reports/{rp_id}/pages'
-        response = get_powerbi_data(access_token, endpoint)
+        response = get_powerbi_data(endpoint)
         data = {
             'WORKSPACE_ID' : ws_id,
             'REPORT_ID' : rp_id,
@@ -118,12 +163,12 @@ def extract_reports_pages(access_token:str, workspaces_ids:list)->pd.DataFrame:
     return df
 
 
-def extract_datasets(access_token:str, workspaces_ids:list)->pd.DataFrame:
+def extract_datasets(workspaces_ids:list)->pd.DataFrame:
     all_data = []
 
     for ws_id in workspaces_ids:
         endpoint = f'{ws_id}/datasets'
-        response = get_powerbi_data(access_token, endpoint)
+        response = get_powerbi_data(endpoint)
         data = {
             'WORKSPACE_ID' : ws_id,
             'response' : response.get('value')
@@ -143,9 +188,9 @@ def extract_datasets(access_token:str, workspaces_ids:list)->pd.DataFrame:
     return df
 
 
-def extract_datasets_info_queries(access_token:str, workspaces_ids:list, dax_query:str)->pd.DataFrame:
+def extract_datasets_info_queries(workspaces_ids:list, dax_query:str)->pd.DataFrame:
     
-    df_datasets = extract_datasets(access_token, workspaces_ids)
+    df_datasets = extract_datasets(workspaces_ids)
     
     workspaces_reports_ids = df_datasets[['WORKSPACE_ID', 'DATASET_ID']].to_dict(orient='records')
 
@@ -155,7 +200,7 @@ def extract_datasets_info_queries(access_token:str, workspaces_ids:list, dax_que
         ws_id = i.get('WORKSPACE_ID')
         ds_id = i.get('DATASET_ID')
         
-        data = execute_powerbi_dataset_query(access_token, ws_id, ds_id, dax_query)
+        data = execute_powerbi_dataset_query(ws_id, ds_id, dax_query)
 
         all_data.append(data)
 
@@ -164,4 +209,5 @@ def extract_datasets_info_queries(access_token:str, workspaces_ids:list, dax_que
     old_columns_name = df.columns
     new_columns_name = {i: re.sub(r'\[([^\]]+)\]', r'\1', i) for i in old_columns_name}
     df = df.rename(columns=new_columns_name)
+    
     return df
--- src/sharepoint_loader.py
+++ src/sharepoint_loader.py
@@ -2,36 +2,74 @@ import os
 import requests
 import pandas as pd
 from io import BytesIO
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_sharepoint_token()->str:
+    TENANT_ID = os.getenv('AZURE_APP_TENANT_ID')
+    CLIENT_ID = os.getenv('AZURE_APP_CLIENT_ID')
+    CLIENT_SECRET = os.getenv('AZURE_APP_CLIENT_SECRET')
+
+    url = f'https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token'
+
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'scope': 'https://graph.microsoft.com/.default'
+    }
+    response = requests.post(url=url, data=payload)
+    if response.status_code == 200:
+        response_json = response.json()
+        data = response_json['access_token']
+        return data
+    else:
+        raise Exception(f'Failed to obtain token: {response.status_code} - {response.text}')
+
+
 
 def get_sharepoint_site_id(access_token:str, site_name:str)->str:
+
     url = f'https://graph.microsoft.com/v1.0/sites?search={site_name}'
+    
     headers = {
         'Authorization' : f'Bearer {access_token}'
     }
+    
     response = requests.get(url, headers=headers)
+    
     if response.status_code == 200:
         site_id = response.json()['value'][0]['id']
         return site_id
+    
     else:
         raise Exception(f'Error to get Sharepoint site id: {response.status_code} - {response.text}')
     
 
 def get_sharepoint_drive_id(access_token:str, site_id:str)->str:
+
     url = f'https://graph.microsoft.com/v1.0/sites/{site_id}/drives'
+    
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
+    
     response = requests.get(url, headers=headers)
+    
     if response.status_code == 200:
         drive_id = response.json()['value'][0]['id']
         return drive_id
+    
     else:
         raise Exception(f'Error to get Sharepoint drive id: {response.status_code} - {response.text}')
     
 
-def load_to_sharepoint(access_token:str, sharepoint_site:str, sharepoint_folder:str, input_df:pd.DataFrame, file_name:str):
-    
+def load_to_sharepoint(sharepoint_site:str, sharepoint_folder:str, input_df:pd.DataFrame, file_name:str):
+    access_token = get_sharepoint_token()
+
     site_id = get_sharepoint_site_id(access_token, os.path.basename(sharepoint_site))
+    
     drive_id = get_sharepoint_drive_id(access_token, site_id)
 
     csv_buffer = BytesIO()
