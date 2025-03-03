1ee6f541b5766463fba9bc5657364f40d96efdc0
lamorasjr <lamorasjr@gmail.com>
2025-03-03 22:27:58 +0100

    refactored repository schema for latest code version

--- README.md
+++ README.md
@@ -2,61 +2,40 @@
 
 This project automates the documentation process for Power BI reports and semantic models across multiple workspaces in the Power BI service.
 
-### Key features include:
-- **Extraction of structural data from components in Power BI Premium Workspaces:** For each defined workspace, the script extracts data from various components, including Workspaces, Reports, Report Pages, Semantic Models, Tables, Measures, Relationships, Calculation Groups, and Calculation Items.
-
-- **Data Storage:** The extracted metadata is saved as CSV files in a SharePoint folder, enabling centralized access.
-
-- **Data Catalog Management:** A Power BI integration is provided through a ready-made template, allowing for detailed reporting and analysis.
-
-
-<!-- <img src=assets\pj_diagram2.png alt="pj_diagram" width="100%"/> -->
-
-
-## Table of Contents
-- [Requirements](#requirements)
-- [Setup](#setup)
-  - [Clone the repository](#1-clone-the-repository)
-  - [Activate the Python version](#2-activate-the-python-version-for-the-project)
-  - [Install dependencies:](#3-install-dependencies-and-run-the-application)
-    - [Option 1: Option 2: Run with Pyenv and Poetry](#option-2-run-with-pyenv-and-poetry)
-    - [Option 2: Run with pip (without Poetry)](#option-3-run-with-pip-without-poetry)
-- [Run the project](#to-run-the-project)
 
 ## Requirements
 
 - **Python 3.12 or greater**
-- **Power BI workspace with Premium or Fabric license**.
+- Power BI workspace with **Premium or Fabric license**.
 - **[Microsoft Entra App](https://learn.microsoft.com/en-us/power-bi/developer/embedded/register-app?tabs=customers)** with at least one of the following scopes:
-  - **Power BI Service**:
+  - **Power BI Service** - to extract Power BI metadata:
     - `Workspace.Read.All`
     - `Workspace.ReadWrite.All`
-  - **Microsoft Graph**:
+  - **Microsoft Graph** - to upload to Sharepoint:
     - `User.Read`
     - `Files.ReadWrite.All`
     - `Sites.ReadWrite.All`
-- **Power BI Desktop** (Required only for report templates)
 
 
 ## Setup
-### 1. Clone the repository:
-Clone the repository to your local machine:
+### 1. Clone thiss repository:
+
 ```bash
 git clone https://github.com/lamorasjr/powerbi-catalog-extractor.git
-
+```
+```bash
 cd powerbi-catalog-extractor
 ```
 
 ### 2. Create the .env File:
 * A `.env` file is required to run the project.
 * Create a `.env` file in the root directory of the project.
-* Copy the content from .env-example and update it with your Microsoft Entra app credentials, source workspaces ids, and details of the target sharepoint.
+* Copy the content from .env-example
+* Update it with your Microsoft Entra app credentials and details of the target Sharepoint folder.
 
-### 3. Install Dependencies and Run the Application
+### 3. Install the project dependencies
 
-You can run the application using one of the following methods:
-
-### Option 1: Run with Pyenv and Poetry (Recommended)
+### Option 1: Pyenv and Poetry (Recommended)
 
 #### 1. Install Python with Pyenv:
 Install the required Python version (3.12.6) with Pyenv:
@@ -64,7 +43,7 @@ Install the required Python version (3.12.6) with Pyenv:
 pyenv install 3.12.6
 ```
 
-#### 2. Activate the Python version for the project:
+#### 2. Activate the Python version required:
 ```bash
 pyenv local 3.12.6
 ```
@@ -78,10 +57,6 @@ poetry install
 #### 4. Activate the Poetry virtual environment:
 ```bash
 poetry shell
-```
-
-```bash
-poetry run python src/main.py
 ```
 
 ### Option 2: Run with pip
@@ -102,13 +77,13 @@ python -m venv .venv
 pip install -r requirements.txt
 ```
 
-## Run the Project
+## 4. Run the Project
 #### 1. With Poetry:
 ```bash
-poetry run python src/main.py
+poetry run python main.py
 ```
 
 #### 2. With Pip
 ```bash
-python src/main.py
+python main.py
 ```
---
+++ main.py
@@ -0,0 +1,102 @@
+import os
+import logging
+from dotenv import load_dotenv
+from src.extract_dax_info_tables import extract_datasets_dax_info
+from src.extract_powerbi_api import (
+    get_powerbi_access_token, 
+    extract_workspaces_ids, 
+    extract_workspaces_data, 
+    extract_datasets_data, 
+    extract_reports_data, 
+    extract_reports_pages
+)
+from src.transformer import (
+    transform_workspaces, 
+    transform_reports, 
+    transform_report_pages, 
+    transform_datasets, 
+    resolve_workspaces_datasets_list, 
+    transform_relationships_info, 
+    transform_tables_info,
+    transform_columns_info,
+    transform_measures_info,
+    transform_calc_groups
+)
+from src.loader import get_sharepoint_access_token, load_csv_to_sharepoint, export_dataframes_to_excel
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='{asctime} - {levelname} - {message}',
+    style='{',
+    datefmt='%Y-%m-%d %H:%M'
+)
+
+# Load variables from .env file
+load_dotenv()
+
+# Read variables from .env file
+PBI_TENANT_ID = os.getenv("PBI_TENANT_ID")
+PBI_CLIENT_ID = os.getenv("PBI_CLIENT_ID")
+PBI_CLIENT_SECRET = os.getenv("PBI_CLIENT_SECRET")
+SHAREPOINT_SITE_URL = os.getenv("SHAREPOINT_SITE_URL")
+SHAREPOINT_RELATIVE_URL = os.getenv("SHAREPOINT_RELATIVE_URL")
+LOCAL_OUTPUT_DIR = os.getenv("LOCAL_OUTPUT_DIR")
+
+def main(load_type=None):  
+    try:
+        logging.info("Starting Power BI Docs extractor...")
+
+        # Get access token for Power BI Rest API
+        pbi_token = get_powerbi_access_token(PBI_TENANT_ID, PBI_CLIENT_ID, PBI_CLIENT_SECRET)
+
+        # Extract list of workspaces ids from Power BI Rest API
+        workspaces_ids = extract_workspaces_ids(pbi_token)
+        
+        # Extract data from Power BI Rest API
+        logging.info("Extracting workspaces data...")
+        workspaces_data = extract_workspaces_data(pbi_token, workspaces_ids)
+        
+        logging.info("Extracting report data...")
+        reports_data = extract_reports_data(pbi_token, workspaces_ids)
+        reports_pages_data = extract_reports_pages(pbi_token, reports_data)
+
+        logging.info("Extracting semantic models data...")
+        datasets_data = extract_datasets_data(pbi_token, workspaces_ids)    
+
+        # Extract data from Dax Studio CDM
+        workspaces_datasets_list = resolve_workspaces_datasets_list(datasets_data, workspaces_data)
+        datasets_info_data = extract_datasets_dax_info(PBI_TENANT_ID, PBI_CLIENT_ID, PBI_CLIENT_SECRET, workspaces_datasets_list)
+
+        # # Transform data and prepare for load
+        logging.info("Transforming and preparing data for load...")
+        workspaces_df = transform_workspaces(workspaces_data)    
+        reports_df = transform_reports(reports_data, datasets_data, workspaces_data)
+        reports_pages_df = transform_report_pages(reports_pages_data, reports_data, workspaces_data)
+        datasets_df = transform_datasets(datasets_data, workspaces_data)
+        tables_df = transform_tables_info(datasets_info_data)
+        columns_df = transform_columns_info(datasets_info_data)
+        measures_df = transform_measures_info(datasets_info_data)
+        calc_groups_df = transform_calc_groups(datasets_info_data)
+        relationships_df = transform_relationships_info(datasets_info_data)
+
+        # Load prepared data to Sharepoint
+        logging.info("Loading data to target object storage...")
+        dataframes = [workspaces_df, reports_df, reports_pages_df, datasets_df, tables_df, columns_df, measures_df, calc_groups_df, relationships_df]
+        sheet_names = ["Workspaces", "Reports", "Reports Pages", "Semantic Models", "Tables", "Columns", "Measures", "Calculation Groups", "Relationships"]
+        file_name = "PowerBI_Docs.xlsx"
+        
+        if load_type == "local":
+            file_name = os.path.join(LOCAL_OUTPUT_DIR, "PowerBI_Docs.xlsx")
+            export_dataframes_to_excel(file_name, dataframes, sheet_names)
+            logging.info("Succesfully completed the Power BI Docs extraction.")
+        else:
+            sp_token = get_sharepoint_access_token(PBI_TENANT_ID, PBI_CLIENT_ID, PBI_CLIENT_SECRET)
+            load_csv_to_sharepoint(sp_token, SHAREPOINT_SITE_URL, SHAREPOINT_RELATIVE_URL, file_name, dataframes, sheet_names)
+
+            logging.info("Succesfully completed the Power BI Docs extraction.")
+    
+    except Exception as e:
+        logging.error(f"Critical error: {e}")
+
+if __name__ == "__main__":
+    main()
